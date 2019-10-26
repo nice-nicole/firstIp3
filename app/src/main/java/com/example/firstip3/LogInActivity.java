5624217f09f83939fb583080e708d3ed9b555526
nice <you@example.com>
2019-10-26 19:25:02 +0200

    wrote codes for login page

--- app/src/main/java/com/example/firstip3/LogInActivity.java
+++ app/src/main/java/com/example/firstip3/LogInActivity.java
@@ -2,15 +2,33 @@ package com.example.firstip3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LogInActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
 
+    @BindView(R.id.crAcc2) TextView crAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        ButterKnife.bind(this);
+        crAccount.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == crAccount){
+            Intent goToMain= new Intent(LogInActivity.this, MainActivity.class);
+            startActivity(goToMain);
+            finish();
+        }
     }
 }
