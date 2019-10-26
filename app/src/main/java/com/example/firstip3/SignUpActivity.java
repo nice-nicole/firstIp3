c9f1e19186f388c536a00b3ef08ac2d9031fe16f
nice <you@example.com>
2019-10-26 20:00:32 +0200

    wrote codes of sign up class

--- app/src/main/java/com/example/firstip3/SignUpActivity.java
+++ app/src/main/java/com/example/firstip3/SignUpActivity.java
@@ -2,13 +2,38 @@ package com.example.firstip3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.name) EditText name;
+    @BindView(R.id.email2) EditText email2;
+    @BindView(R.id.pass2) EditText pass2;
+
+    @BindView(R.id.crAcc) Button crAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ButterKnife.bind(this);
+        crAcc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == crAcc){
+            Intent goToMain2= new Intent(SignUpActivity.this, MainActivity.class);
+            startActivity(goToMain2);
+            finish();
+        }
     }
 }
