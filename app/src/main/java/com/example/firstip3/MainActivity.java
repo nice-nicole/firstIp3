14afce5b22ad041c66239827533681c8e32a9062
nice <you@example.com>
2019-10-27 16:35:31 +0200

    added options of displaying name of the user after signing up in the main activity class

--- app/src/main/java/com/example/firstip3/MainActivity.java
+++ app/src/main/java/com/example/firstip3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firstip3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,38 +10,73 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth authProtocol;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        authProtocol = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser person = firebaseAuth.getCurrentUser();
+                if(person != null ){
+                getSupportActionBar().setTitle("Dear " + person.getDisplayName() + ", Thank you for joining our community!...");
+                }
+            }
+        };
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inf = getMenuInflater();
-        inf.inflate(R.menu.menu_main,menu);
+        inf.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
 
     }
 
-    private void logout(){
+    private void logout() {
         FirebaseAuth.getInstance().signOut();
-        Intent returnToLogin = new Intent(MainActivity.this,LogInActivity.class);
+        Intent returnToLogin = new Intent(MainActivity.this, LogInActivity.class);
         returnToLogin.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(returnToLogin);
         finish();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id= item.getItemId();
-        if(id == R.id.actions){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.actions) {
             logout();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        authProtocol.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            authProtocol.removeAuthStateListener(mAuthListener);
+        }
+
+    }
 }
+
