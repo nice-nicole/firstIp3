22a2d128a97d6082fa55b5d83dca418fe3d3c057
nice <you@example.com>
2019-10-27 14:57:23 +0200

    added codes for logout in main activity

--- app/src/main/java/com/example/firstip3/MainActivity.java
+++ app/src/main/java/com/example/firstip3/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.firstip3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inf = getMenuInflater();
+        inf.inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    private void logout(){
+        FirebaseAuth.getInstance().signOut();
+        Intent returnToLogin = new Intent(MainActivity.this,LogInActivity.class);
+        returnToLogin.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(returnToLogin);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id= item.getItemId();
+        if(id == R.id.actions){
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
