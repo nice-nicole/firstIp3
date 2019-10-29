f9aa08d19017805b3689c6b528f168ead39c9580
nice <you@example.com>
2019-10-29 14:05:43 +0200

    changes

--- app/src/main/java/com/example/firstip3/ui/MainActivity.java
+++ app/src/main/java/com/example/firstip3/ui/MainActivity.java
@@ -5,17 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.firstip3.Constants;
 import com.example.firstip3.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private DatabaseReference mSearchedLocationReference;
+    private ValueEventListener mSearchedLocationReferenceListener;
+
+    @BindView(R.id.findNewsbutton)
+    Button mFindNewsButton;
+        @BindView(R.id.locationEditText)
+        EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
+
+    @BindView(R.id.savedNewsButton) Button mSavedNewsButton;
 
     private FirebaseAuth authProtocol;
     private FirebaseAuth.AuthStateListener mAuthListener;
@@ -37,6 +61,58 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+        mFindNewsButton.setOnClickListener(this);
+        mSavedNewsButton.setOnClickListener(this);
+
+        mSearchedLocationReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
+
+        mSearchedLocationReferenceListener= mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot locationSnapshot : dataSnapshot.getChildren()){
+                    String location = locationSnapshot.getValue().toString();
+                    Log.d("Locations updated", "location: " + location);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mFindNewsButton){
+//                    String location= mLocationEditText.getText().toString();
+
+//                    saveLocationToFirebase(location);
+
+//                    if(!(location).equals("")) {
+//                        addToSharedPreferences(location);
+//                    }
+
+            Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+            //                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+//                    intent.putExtra("location", location);
+            startActivity(intent);
+
+        }
+//        if (v == mSavedNewsButton) {
+//            Intent intent = new Intent(MainActivity.this, SavedNewsListActivity.class);
+//            startActivity(intent);
+//        }
+
+
+    }
+
+    public void saveLocationToFirebase(String location) {
+        mSearchedLocationReference.push().setValue(location);
     }
 
     @Override
